21e2b47194aa088b642d6a6f98630dc9a68f464e
Tanishq9980 <tanishq0564.becse24@chitkara.edu.in>
2025-03-31 10:37:46 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,35 +1,45 @@
-#include <stdio.h>
-void rotateright(int a[],int N,int K)
-{
+void rotateright(int a[],int N,int K){
     K=K%N;
-    int temp[K];
-    for(int i=0;i<K;i++)
-       {
-        temp[i]=a[N-K+i];
-       }
-    for(int i=N-1;i>K;i++)
-       {
-        a[i]=a[i-K];
-       }
-    for(int i=0;i<K;i++)
-       {
-        a[i]=temp[i];
-       }
+
+   int temp[K];
+
+   for(int i=0;i<K;i++)
+   {
+          temp[i]=a[N-K+i];
+   }
+   
+   for(int i=N-1;i>=K;i--)
+   {
+         a[i]=a[i-K];
+   }
+
+   for(int i=0;i<K;i++)
+   {
+    a[i]=temp[i];
+   }
 }
+
+
 int main()
 {
     int N,K;
+    printf("Enter the size of the array: ");
     scanf("%d",&N);
+     
     int a[N];
-    for (int i=0;i<N;i++){
+    printf("Enter the elements of the array: ");
+    for(int i=0;i<N;i++)
+    {
         scanf("%d",&a[i]);
     }
+    printf("Enter the number of positions to rotate: ");
     scanf("%d",&K);
-     
-     rotateright(a,N,K);
-for(int i=0;i<N;i++)
-{
-     printf("%d",a[i]);
-}
+
+    rotateright(a,N,K);
+
+    for(int i=0;i<N;i++)
+    {
+        printf("%d",a[i]);
+    }
     return 0;
 }
